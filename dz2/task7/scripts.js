fb1d819eb02b1dcf376baa9b578d975ae5cdfc6e
wiergushtrefa <colbert_theht@bk.ru>
2025-01-31 19:26:22 +0300

    some changes

--- dz2/task7/scripts.js
+++ dz2/task7/scripts.js
@@ -1,3 +1,15 @@
-document.getElementById('button').addEventListener('click', function() {
-    let 
-})
+function calculateDiscount(){
+    const amount = parseFloat(document.getElementById('amount').value);
+    let discount = 0;
+     
+     if (amount >= 200 && amount < 300){
+        discount = 0.03; // 3%
+     } else if (amount >= 300 && amount < 500){
+        discount = 0.05; // 5%
+     } else if (amount >=500){
+        discount = 0.07; // 7%
+     }
+     
+     const total = amount - (amount * discount);
+     document.getElementById('result').innerText = 'Сумма к оплате: ${total.toFixed(2)} руб.';
+} 
